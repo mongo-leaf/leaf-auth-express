6725941c05a7e2b7c79f3a303cbef79897a04554
Khaled Romdhane <heiyukidev@gmail.com>
2017-12-16 13:29:40 +0100

    setProvider bug fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let publicRouter = require('express').Router();
 let secureRouter = require('express').Router();
 
 const response = require('dark-snow-response');
-response.setProvider = "leaf-auth-express";
+response.setProvider("leaf-auth-express");
 
 
 publicRouter.use("/",require('./routers/public.router.js'));
